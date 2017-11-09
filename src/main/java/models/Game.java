eb682ed23d1576e21b05ac8a79fde691d9cddcb7
Trent Vasquez <vasquezt@oregonstate.edu>
2017-11-08 17:03:39 -0800

    Added missing curley brace that was removed

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -13,21 +13,22 @@ public class Game {
     //public java.util.List<java.util.List<Card>> cols = new ArrayList<>(4);
     public Player p;
 
-    public Game(){
+    public Game() {
         d = new Deck();
         p = new Player();
     }
 
     public void dealFour() {
-    	System.out.print("inside deal four function");
+        System.out.print("inside deal four function");
         // remove the top card from the deck and add it to a column; repeat for each of the four columns
         int elements = d.deck.size();
-        if(elements >= 4){
-            for(int i = 0; i < 4; i++){
+        if (elements >= 4) {
+            for (int i = 0; i < 4; i++) {
                 p.cols.get(i).add(d.deck.get(0));
                 d.deck.remove(0);
             }
-        }else{
+        } else {
             System.out.print("out of cards");
         }
     }
+}
