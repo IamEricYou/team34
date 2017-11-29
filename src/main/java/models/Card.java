af0679af3ba34f631c508903c162c8e747bfa3df
Trent Vasquez <vasquezt@oregonstate.edu>
2017-11-29 10:34:38 -0800

    Cleaned code, Added new suits/deck, face vals show

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -5,19 +5,34 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
-/**
- * Assignment 1: No changes are needed in this file, but it is good to be aware of for future assignments.
- */
 
 public class Card implements Serializable {
     public final int value;
     public final Suit suit;
+    public String vis;
 
     @JsonCreator
     public Card(@JsonProperty("value") int value, @JsonProperty("suit") Suit suit) {
         this.value = value;
         this.suit = suit;
-
+        vis = Integer.toString(value);
+        if(value > 10) {
+            System.out.println("The value is greater than 10");
+        	if(value == 11) {
+        		vis = "J";
+        	}
+        	else if(value == 12) {
+        		vis = "Q";
+        	}
+        	else if(value == 13) {
+        		vis = "K";
+        	}
+        	else if(value == 14) {
+        		vis = "A";
+        	}
+        }
+        System.out.println("The displayed value should be");
+        System.out.println(this.vis);
     }
 
     public Suit getSuit() {
@@ -28,7 +43,9 @@ public class Card implements Serializable {
         return value;
     }
 
+    
+    // Jack == 11 Queen == 12 King == 13 Ace = 14
     public String toString() {
-        return this.value + this.suit.toString();
+        return this.vis + this.suit.toString();
     }
 }
